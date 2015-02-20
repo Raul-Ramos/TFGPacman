8a459e7cd13c349a3a7f5cc6aaa535f07d2f7aee
MetroGoldyMayer <amphiros@gmail.com>
2015-02-20 20:56:07 +0100

    - Permitidos addons para utilizar ogmo - Cambiado el tamaño de la pantalla 640x480 a 630x660 - Cambiado el tamaño del juego consecuentemente - Diferenciadas paredes y zonas en OGMO - Reformado mapa - Cambiados tilesets - Implementada carga simple del mapa

--- Pacman/source/PlayState.hx
+++ Pacman/source/PlayState.hx
@@ -7,17 +7,37 @@ import flixel.text.FlxText;
 import flixel.ui.FlxButton;
 import flixel.util.FlxMath;
 
+import flixel.addons.editors.ogmo.FlxOgmoLoader;
+import flixel.tile.FlxTilemap;
+import flixel.FlxObject;
+
 /**
  * A FlxState which can be used for the actual gameplay.
  */
 class PlayState extends FlxState
 {
+	
+	private var _map:FlxOgmoLoader;
+	private var _mWalls:FlxTilemap;
+	
 	/**
 	 * Function that is called up when to state is created to set it up. 
 	 */
 	override public function create():Void
 	{
 		super.create();
+		
+		_map = new FlxOgmoLoader(AssetPaths.n1__oel);
+		trace(_map);
+		
+		_mWalls = _map.loadTilemap(AssetPaths.tileset__png, 50, 50, "paredes");
+		trace(_mWalls.getData());
+		_mWalls.loadMap(_mWalls.getData(), AssetPaths.tileset__png, 50, 50, FlxTilemap.AUTO);
+		trace(_mWalls.getData());
+		//_mWalls.loadMap(_map, AssetPaths.tileset__png, 50, 50, FlxTilemap.AUTO);
+		//_mWalls.setTileProperties(1, FlxObject.NONE);
+		//_mWalls.setTileProperties(2, FlxObject.ANY);
+		add(_mWalls);
 	}
 	
 	/**
